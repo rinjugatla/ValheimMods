a73112f9023d14e0242dcc83879b0891d5f3ea78
rinjugatla <rin.jugatla@gmail.com>
2021-02-25 20:19:52 +0900

    SandBoxプロジェクトを追加

---
+++ SandBox/SandBox.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandBox
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class BepInExPlugin : BaseUnityPlugin
+    {
+        public const string PluginGuid = "rin_jugatla.SandBox";
+        public const string PluginName = "SandBox";
+        public const string PluginVersion = "1.0.0";
+        /// <summary>
+        /// デバッグが有効か
+        /// </summary>
+#if Debug
+        private static readonly bool IsDebug = true;
+#else
+        private static readonly bool IsDebug = false;
+#endif
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+        }
+    }
+}
