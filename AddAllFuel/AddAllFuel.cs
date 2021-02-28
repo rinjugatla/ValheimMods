4b1f315822d6395d11daf61c6ad92fcc7e89fe8e
rinjugatla <rin.jugatla@gmail.com>
2021-02-28 18:39:52 +0900

    バージョンを更新

--- AddAllFuel/AddAllFuel.cs
+++ AddAllFuel/AddAllFuel.cs
@@ -15,7 +15,7 @@ namespace AddAllFuel
     {
         public const string PluginGuid = "rin_jugatla.AddAllFuel";
         public const string PluginName = "AddAllFuel";
-        public const string PluginVersion = "1.2.1";
+        public const string PluginVersion = "1.3.0";
         /// <summary>
         /// デバッグが有効か
         /// </summary>
