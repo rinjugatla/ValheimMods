5c4d2a1193add55cdf3a6c16fb4a6dce1437e717
rinjugatla <rin.jugatla@gmail.com>
2021-03-07 13:46:18 +0900

    ストレージソートMODプロジェクトを追加

---
+++ StorageSort/StorageSort.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageSort
+{
+	[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+	public class BepInExPlugin : BaseUnityPlugin
+	{
+		public const string PluginGuid = "rin_jugatla.StorageSort";
+		public const string PluginName = "StorageSort";
+		public const string PluginVersion = "1.0.0";
+		/// <summary>
+		/// デバッグが有効か
+		/// </summary>
+#if Debug
+		private static readonly bool IsDebug = true;
+#else
+		private static readonly bool IsDebug = false;
+#endif
+		/// <summary>
+		/// MODが有効か
+		/// </summary>
+		private static ConfigEntry<bool> IsEnabled;
+		/// <summary>
+		/// 自動アップデート用のNexusID
+		/// </summary>
+		/// <remarks>
+		/// https://www.nexusmods.com/valheim/mods/102
+		/// </remarks>
+		private static ConfigEntry<int> NexusID;
+
+		private void Awake()
+		{
+			IsEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod.");
+			NexusID = Config.Bind<int>("General", "NexusID", -1, "Nexus mod ID for updates.");
+
+			Harmony.CreateAndPatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+		}
+	}
+}
